44d1a0294863019ef950db02565f4cc25418c5c0
WaiKeith <keith110733@gmail.com>
2016-02-11 22:55:57 +0800

    major undone nfc, order undone qr generator, notification bug editacc

---
+++ app/src/main/java/my/edu/chiawaikeith/canteenpos/Domains/Reports.java
@@ -0,0 +1,122 @@
+package my.edu.chiawaikeith.canteenpos.Domains;
+
+/**
+ * Created by cwfei on 3/20/2015.
+ */
+public class Reports {
+
+    private int rice,noodle,snack,bread;
+    private double payment,day1,day2,day3,day4,day5,day6,day7,todayTotal,todayGst;
+
+    public int getRice() {
+        return rice;
+    }
+
+    public void setRice(int rice) {
+        this.rice = rice;
+    }
+
+    public int getNoodle() {
+        return noodle;
+    }
+
+    public void setNoodle(int noodle) {
+        this.noodle = noodle;
+    }
+
+    public int getSnack() {
+        return snack;
+    }
+
+    public void setSnack(int snack) {
+        this.snack = snack;
+    }
+
+    public int getBread() {
+        return bread;
+    }
+
+    public void setBread(int bread) {
+        this.bread = bread;
+    }
+
+    public double getPayment() {
+        return payment;
+    }
+
+    public void setPayment(double payment) {
+        this.payment = payment;
+    }
+
+    public double getDay1() {
+        return day1;
+    }
+
+    public void setDay1(double day1) {
+        this.day1 = day1;
+    }
+
+    public double getDay2() {
+        return day2;
+    }
+
+    public void setDay2(double day2) {
+        this.day2 = day2;
+    }
+
+    public double getDay3() {
+        return day3;
+    }
+
+    public void setDay3(double day3) {
+        this.day3 = day3;
+    }
+
+    public double getDay4() {
+        return day4;
+    }
+
+    public void setDay4(double day4) {
+        this.day4 = day4;
+    }
+
+    public double getDay5() {
+        return day5;
+    }
+
+    public void setDay5(double day5) {
+        this.day5 = day5;
+    }
+
+    public double getDay6() {
+        return day6;
+    }
+
+    public void setDay6(double day6) {
+        this.day6 = day6;
+    }
+
+    public double getDay7() {
+        return day7;
+    }
+
+    public void setDay7(double day7) {
+        this.day7 = day7;
+    }
+
+    public double getTodayTotal() {
+        return todayTotal;
+    }
+
+    public void setTodayTotal(double todayTotal) {
+        this.todayTotal = todayTotal;
+    }
+
+    public double getTodayGst() {
+        return todayGst;
+    }
+
+    public void setTodayGst(double todayGst) {
+        this.todayGst = todayGst;
+    }
+}
