1976cbe25ba12197530b12884df843a340aa2f1f
WaiKeith <keith110733@gmail.com>
2016-02-13 21:43:30 +0800

    minor undone nfc undone qr generator, notification bug editacc

--- app/src/main/java/my/edu/chiawaikeith/canteenpos/Activities/FoodDetails.java
+++ app/src/main/java/my/edu/chiawaikeith/canteenpos/Activities/FoodDetails.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.gordonwong.materialsheetfab.MaterialSheetFab;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -22,6 +23,7 @@ import java.text.ParseException;
 import java.util.Calendar;
 import java.util.HashMap;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import my.edu.chiawaikeith.canteenpos.Adapters.FoodAdapter;
 import my.edu.chiawaikeith.canteenpos.Domains.Foods;
 import my.edu.chiawaikeith.canteenpos.Domains.Transactions;
@@ -37,6 +39,7 @@ public class FoodDetails extends BaseActivity implements View.OnClickListener {
     private JSONArray mJsonArray;
     private MaterialSheetFab materialSheetFab;
     public Foods food = new Foods();
+    private CircleImageView foodPic;
     public static final String KEY_FOOD = "food";
     Transactions transaction = new Transactions();
     public Integer acc_id,transac_id,newTransac_id;
@@ -44,12 +47,14 @@ public class FoodDetails extends BaseActivity implements View.OnClickListener {
 
     final static String INSERT_URL = "http://dinpos.comlu.com/OrderLines/insert_order_line.php";
     final static String GET_URL = "http://dinpos.comlu.com/Transactions/get_transaction.php";
+    final static String GETFOOD_URL = "http://dinpos.comlu.com/Foods/retrieve_foods.php";
     final static String KEY_ACCOUNT_ID = "acc_id";
     final static String KEY_TRANSAC_ID = "transac_id";
     final static String KEY_FOOD_ID = "food_id";
     final static String KEY_ITEMQTY = "item_qty";
     final static String KEY_SINGLE_PRICE = "single_price";
     final static String KEY_ORDER_DATETIME = "order_date_time";
+    final static String KEY_FOOD_IMAGE_PATH = "food_image_path";
     final static String KEY_TOTAL_GST = "total_gst";
     final static String KEY_ORDER_STATUS = "order_status";
     final static String KEY_PAYMENT_AMOUNT = "payment_amount";
@@ -67,6 +72,7 @@ public class FoodDetails extends BaseActivity implements View.OnClickListener {
         foodName = (TextView)findViewById(R.id.food_name);
         price = (TextView)findViewById(R.id.food_price);
         quantity = (TextView)findViewById(R.id.qty);
+        foodPic = (CircleImageView)findViewById(R.id.image_food);
 
         setSupportActionBar(toolBar);
         assert getSupportActionBar() != null;
@@ -90,7 +96,7 @@ public class FoodDetails extends BaseActivity implements View.OnClickListener {
         food = (Foods) getIntent().getSerializableExtra(FoodAdapter.KEY_FOOD);
         initValues();
         getTransaction();
-
+        getFood();
 
     }
 
@@ -166,6 +172,71 @@ public class FoodDetails extends BaseActivity implements View.OnClickListener {
             }
         }
 
+    }
+
+    public void getFood(){
+        new getFood().execute();
+    }
+
+    public class getFood extends AsyncTask<String, Void, String> {
+        ProgressDialog loading;
+        RequestHandler rh = new RequestHandler();
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //loading = ProgressDialog.show(getActivity(), "Uploading...", null, true, true);
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            super.onPostExecute(json);
+            convertJson1(json);
+            extractJsonData1();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            HashMap<String, String> data = new HashMap<>();
+            data.put(KEY_FOOD_ID,  String.valueOf(food.getFood_id()));
+
+            return rh.sendPostRequest(GETFOOD_URL, data);
+        }
+    }
+
+    private void convertJson1(String json) {
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            mJsonArray = jsonObject.getJSONArray(BaseActivity.JSON_ARRAY);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void extractJsonData1() {
+
+        for (int i = 0; i < mJsonArray.length(); i++) {
+            try {
+                JSONObject jsonObject = mJsonArray.getJSONObject(i);
+                //transaction = new Transactions();
+
+                food.setFood_id((jsonObject.getInt(KEY_FOOD_ID)));
+                food.setFood_name(jsonObject.getString(KEY_FOOD_NAME));
+                food.setFood_image_path(jsonObject.getString(KEY_FOOD_IMAGE_PATH));
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            loadFoodImage();
+        }
+
+    }
+
+    private void loadFoodImage() {
+        if(food.getFood_image_path() != ""){
+            ImageLoader.getInstance().displayImage(food.getFood_image_path(), foodPic, options);}
     }
 
     @Override
